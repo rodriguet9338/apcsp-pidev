39feda869ffbf1e49096a3ef44e3de18271fb8b0
rodriguet9338 <trod888@gmail.com>
2019-05-27 22:05:56 -0700

    Create pointers.c

---
+++ cdemo/pointers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int* ptrtoa;
+  
+  ptrtoa = &a;
+  
+  a = 5;
+  printf("The value of a is %d\n", a);
+  
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
+  
+  printf(The value of ptrtoa is %d\n", ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %d\n", &a);
+  
+  float x = 1;
+  float y = 2;
+  
+  printf("The value of x is %f\n", x);
+  print f("The value of y is %f\n"), y);
+  printf("The address of x is %f\n", &x);
+  printf("The address of y is %f\n", &"y);
+  
+  float* ptrtox = &x;
+  float* ptrtoy = &y;
+  
+  temp = *ptrtox;
+  x = *ptrtoy;
+  y = temp;
+  
+  printf("The values of x and y have now swapped.\n");
+  printf("The value of x is now %f, and the value of y is now %f. \n", x, y);
+}
